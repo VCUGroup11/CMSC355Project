d3cfe0a0b842bd00fcd8499d9840c542f3d414cf
tandalesc <tandalesc@vcu.edu>
2014-11-16 14:11:06 -0500

    Generalized FileIO
    Static calls can now be made (checked during implementation, not in
    back-end), and now FileIO uses Maps generated from the header of the CSV
    file being read to read the correct elements into each Student object.
    
    Will be made more robust later on

--- FileIO.java
+++ FileIO.java
@@ -1,7 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -9,23 +10,21 @@ public class FileIO {
 	private Scanner s;
 	
 	private File currentFile;
-
-	//TODO eventually utilize a Map to switch between formats
-	//public static final HashMap DEFAULT_FORMAT = new HashMap<String,Integer>();
 	
-	//Lists default format
-	
-	/*public static final String[] DEFAULT_FILE_FORMAT = 
-		{"lastname","firstname",
-			"idnum","grade",
-			"majorGPA","totalGPA",
-			"majorCrd","upperCrd","totalCrd"};
-			
-			*/
-
-	/**
-	 * Default Constructor, do not recommend using
+	/*
+	 * currentTemplate provides a way to go between a header string and an index corresponding to the right entry
 	 */
+	private Map<String, Integer> currentTemplate = new HashMap<>();
+	
+	/**
+	 * Used to classify elements of each template
+	 */
+	public static final String[] DEFAULT_FILE_FORMAT = 
+		{"ln","fn",
+			"idnum","grade",
+			"mGPA","tGPA",
+			"mCrd","uCrd","tCrd"};
+
 	public FileIO() {
 		start();
 	}
@@ -50,7 +49,11 @@ public class FileIO {
 			e.printStackTrace();
 		}
 	}
-	private void stop() {
+	/**
+	 * Call to prevent resource leaks. Will be automated in a future revision
+	 * TODO
+	 */
+	public void stop() {
 		if(s!=null) {
 			s.close();
 		}
@@ -63,31 +66,101 @@ public class FileIO {
 		return currentFile;
 	}
 	
-	//VERY basic readFile function
+	/**
+	 * Set template to one defined  by a csv header. DEFAULT_FILE_FORMAT provides the order and the check strings used
+	 * @param csvHeader
+	 * String array containing the comma separated values that need to be mapped 
+	 */
+	public void setTemplate(String... csvHeader) {
+		setTemplate(getTemplate(csvHeader));
+	}
+	public void setTemplate(Map<String,Integer> t) {
+		currentTemplate = t;
+	}
+	public static Map<String,Integer> getTemplate(String... csvHeader) {
+		HashMap<String, Integer> t = new HashMap<>();
+		
+		for(String e:csvHeader) {
+			t.put(e, Arrays.binarySearch(DEFAULT_FILE_FORMAT, e));
+		}
+		
+		return t;
+	}
+	public Map<String, Integer> getTemplate() {
+		return currentTemplate;
+	}
+	
+	/**
+	 * Reads files based on FileIO object. Use for reading multiple files with the same object,
+	 * or with different templates
+	 * @return
+	 * StudentList containing all the students in the file.
+	 */
 	public StudentList readFile() {
 		StudentList sl = new StudentList();
-		s.nextLine(); //skip first template line
+		
+		String[] template = s.nextLine().split(","); //read template line (first line of document)
+		setTemplate(template);
+		
 		while(s.hasNextLine()) {
 			String[] data = s.nextLine().split(","); //raw data containing all comma seperated values of line
 			Student p = new Student();
 			
-			p.setFirstName(data[0]);
-			p.setLastName(data[1]);
-			p.setIdNum(data[2]);
-			p.setGrade(data[3]);
+			p.setFirstName(data[currentTemplate.get("fn")]);
+			p.setLastName(data[currentTemplate.get("ln")]);
+			p.setIdNum(data[currentTemplate.get("idNum")]);
+			p.setGrade(data[currentTemplate.get("grade")]);
 			
-			p.setMajorGPA(Float.parseFloat(data[4]));
-			p.setTotalGPA(Float.parseFloat(data[5]));
-			p.setMajorCrd(Integer.parseInt(data[6]));
-			p.setUpperCrd(Integer.parseInt(data[7]));
-			p.setTotalCrd(Integer.parseInt(data[8]));
+			p.setMajorGPA(Float.parseFloat(data[currentTemplate.get("mGPA")]));
+			p.setTotalGPA(Float.parseFloat(data[currentTemplate.get("tGPA")]));
+			p.setMajorCrd(Integer.parseInt(data[currentTemplate.get("mCrd")]));
+			p.setUpperCrd(Integer.parseInt(data[currentTemplate.get("uCrd")]));
+			p.setTotalCrd(Integer.parseInt(data[currentTemplate.get("tCrd")]));
 			
 			sl.add(p);
 		}
-		//TODO optimize and generalize
+		
 		return sl;
 	}
-	
+	/**
+	 * Static implementation of FileIO's readFile command. Will be deprecated soon and merged with non-static implementation
+	 * @param f
+	 * File to be read. Template is extracted from this file.
+	 * @return
+	 * Student list containing all recognized information from file
+	 * @throws FileNotFoundException 
+	 * Scanner calls are not checked in this implementation
+	 */
+	public static StudentList readFile(File f) throws FileNotFoundException {
+		StudentList sl = new StudentList();
+		Scanner m = new Scanner(f);
+		String[] template = m.nextLine().split(","); //read template line (first line of document)
+		Map<String,Integer> ct = getTemplate(template); //turn template line into Map to use as template
+		
+		while(m.hasNextLine()) {
+			String[] data = m.nextLine().split(","); //raw data containing all comma seperated values of line
+			Student p = new Student();
+			
+			/*
+			 * Sets each part of student equal to the correct element of the data string
+			 * Matches are determined by the map
+			 */
+			p.setFirstName(data[ct.get("fn")]);
+			p.setLastName(data[ct.get("ln")]);
+			p.setIdNum(data[ct.get("idNum")]);
+			p.setGrade(data[ct.get("grade")]);
+			
+			p.setMajorGPA(Float.parseFloat(data[ct.get("mGPA")]));
+			p.setTotalGPA(Float.parseFloat(data[ct.get("tGPA")]));
+			p.setMajorCrd(Integer.parseInt(data[ct.get("mCrd")]));
+			p.setUpperCrd(Integer.parseInt(data[ct.get("uCrd")]));
+			p.setTotalCrd(Integer.parseInt(data[ct.get("tCrd")]));
+			
+			sl.add(p);
+		}
+		m.close();
+		return sl;
+	}
 	
 	
 	
