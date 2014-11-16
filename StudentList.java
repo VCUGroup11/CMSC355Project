feadef14cb362c937fb103f4553b70da7ee407d6
tabinonf <tabinonf@vcu.edu>
2014-11-15 20:53:05 -0500

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -1,41 +1,120 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-//
-//Still needs method to remove a student from the list.
-//
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 
-public class StudentList {
+public class StudentList implements Set<Student> {
 	
-	private ArrayList<Student> studentList;// = new ArrayList<Student>();
+	private Set<Student> studentList;
 	
 	public StudentList() {
-		this.setStudentList(new ArrayList<Student>(25));
+		this.setStudentList(new HashSet<Student>());
 	}
 	
-	public StudentList(ArrayList<Student> list) {
-		this.setStudentList(list);
+	public StudentList(Set<Student> list) {
+		this.setStudentList(new HashSet<Student>(list));
 	}
 
-	public ArrayList<Student> getStudentList() {
+	public Set<Student> getStudentList() {
 		return studentList;
 	}
 
-	public void setStudentList(ArrayList<Student> studentList) {
+	public void setStudentList(Set<Student> studentList) {
 		this.studentList = studentList;
 	}
 	
 	public void addStudent(Student s) {
-		for (Student s1 : this.studentList) {
-			if (s1.compareTo(s) == 0) {
-				System.out.println("Student: " + s.toString() + " \nIs already in the list of students");
-				return;
-			}
+		System.out.println("Adding student: " + s.toString() + " \nto the list of students");
+		if (this.contains(s)) {
+			System.out.println("Student: " + s.toString() + " \nIs already in the list of students");
+			return;
 		}
-		this.studentList.add(s);
-		Collections.sort(this.studentList);
+		else {
+			this.add(s);
+		}
 		System.out.println("Student: " + s.toString() + " \nhas been added to the list of students");
+	}//End addStudent()
+	
+	public void removeStudent(Student s) {
+		System.out.println("Removing student: " + s.toString() + " \nfrom the list of students");
+		if (this.contains(s)) {
+			this.remove(s);
+			System.out.println("Student: " + s.toString() + " \nwas removed from the list of students");
+			return;
+		}
+		else {
+			System.out.println("Student: " + s.toString() + " \nwas not found in the list of students");
+		}
+	}//End removeStudent()
+
+	@Override
+	public boolean add(Student s) {
+		this.studentList.add(s);
+		return this.contains(s);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends Student> s) {
+		return this.studentList.addAll(s);
+	}
+
+	@Override
+	public void clear() {
+		this.studentList.clear();		
+	}
+
+	@Override
+	public boolean contains(Object s) {
+		return this.studentList.contains(s);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> s) {
+		return this.studentList.containsAll(s);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.studentList.isEmpty();
+	}
+
+	@Override
+	public Iterator<Student> iterator() {
+		return this.studentList.iterator();
+	}
+
+	@Override
+	public boolean remove(Object s) {
+		this.studentList.remove(s);
+		return !this.contains(s);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> s) {
+		this.studentList.removeAll(s);
+		return !this.containsAll(s);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> s) {
+		this.studentList.retainAll(s);
+		return this.containsAll(s);
+	}
+
+	@Override
+	public int size() {
+		return this.studentList.size();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return this.studentList.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] s) {
+		return this.studentList.toArray(s);
 	}
 
 }
